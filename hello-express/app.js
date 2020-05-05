3e197219c75c8c7d4de65f335350a6121fdd6a9f
JIN <a1824jj@aiit.ac.jp>
2020-05-05 07:59:28 +0000

    npm install express - hello world

---
+++ hello-express/app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+app.get("/",(req, res)=>{
+  //res.writeHead(200);
+  //res.write("Hello World");
+
+  res.status(200).send("Hello World.");
+
+});
+app.listen(3000);
