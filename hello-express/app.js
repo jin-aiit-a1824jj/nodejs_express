61dbcb3ffc747e58a58019e03679f9f661527b07
JIN <a1824jj@aiit.ac.jp>
2020-05-05 08:18:36 +0000

    using express Router

--- hello-express/app.js
+++ hello-express/app.js
@@ -6,8 +6,23 @@ app.use(require("./logger"));
 app.get("/",(req, res)=>{
   //res.writeHead(200);
   //res.write("Hello World");
-
   res.status(200).send("Hello World.");
-
 });
+
+app.get("/home/index",(req, res)=>{
+  res.status(200).send("Ok.");
+});
+
+app.get("/guest/:id",(req, res)=>{
+  console.log(req.params.id);
+  res.status(200).send("Ok. /guest");
+});
+
+app.get("/guest/:id?",(req, res)=>{
+  console.log(req.params.ids);
+  res.status(200).send("Ok. /guest/?");
+});
+
+app.use("/user", require("./user"));
+
 app.listen(3000);
