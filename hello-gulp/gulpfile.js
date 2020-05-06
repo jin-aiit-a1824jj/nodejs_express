3c4d5f283fe47845d0d27d70bfa1cf187acd12b5
JIN <a1824jj@aiit.ac.jp>
2020-05-06 03:03:51 +0000

    gulp - file control

--- hello-gulp/gulpfile.js
+++ hello-gulp/gulpfile.js
@@ -1,4 +1,39 @@
 var gulp = require("gulp");
+var del = require("del");
+var rename = require("gulp-rename");
+var concat = require("gulp-concat");
+
+gulp.task("copy", (done)=>{
+  gulp.src("./src/sample1.txt").pipe(gulp.dest("./dist"));
+  done();
+});
+
+gulp.task("delete", (done)=>{
+    del(["./dist/*"], {force: true})
+      .then((res)=>{
+        console.log("success->  " + res);
+        done();
+      })
+      .catch((err)=>{
+        console.log("failure->  " +err);
+        done();
+      });
+});
+
+gulp.task("rename", (done)=>{
+  gulp.src("./src/sample1.txt")
+  //.pipe(rename("hoge.txt"))
+  .pipe(rename({suffix: ".min"}))
+  .pipe(gulp.dest("./dist"));
+  done();
+});
+
+gulp.task("concat",(done)=>{
+  gulp.src(["sample1.txt", "sample2.txt"],{ cwd: "./src"})
+  .pipe(concat("bundle.txt"))
+  .pipe(gulp.dest("./dist"));
+  done();
+});
 
 gulp.task("a", (done)=>{
   console.log("a");
@@ -10,7 +45,7 @@ gulp.task("b", (done)=>{
   done();
 });
 
-gulp.task("default", gulp.series("a","b",(done)=>{
+gulp.task("default", gulp.series("copy","delete","rename","concat","a","b",(done)=>{
   console.log("default");
   done();
 }));
