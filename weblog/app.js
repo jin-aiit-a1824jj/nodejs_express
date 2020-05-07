c10fbf44d43085d8159f8315fef62f2109607b99
JIN <a1824jj@aiit.ac.jp>
2020-05-07 01:53:24 +0000

    log4js - systemlog && make middleware

--- weblog/app.js
+++ weblog/app.js
@@ -1,6 +1,6 @@
+var systemLogger = require("./lib/log/systemlogger");
 var express = require("express");
 var app = express();
-//var logger = require("./lib/log/logger").console;
 
 app.set("view engine", "ejs");
 app.disable("x-powered-by");
@@ -9,5 +9,6 @@ app.use("/public", express.static( __dirname + "/public/" + (process.env.NODE_EN
 
 app.use("/", require("./routes/index.js"));
 
+app.use(systemLogger());
+
 app.listen(3000);
-//logger.info("start.");
