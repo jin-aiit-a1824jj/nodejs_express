f6cdf22f021acc309a7ef2c67b93bebfd479442f
JIN <a1824jj@aiit.ac.jp>
2020-05-07 08:23:39 +0000

    new post input complete

--- weblog/routes/account.js
+++ weblog/routes/account.js
@@ -1,3 +1,5 @@
+var { CONNECTION_URL, OPTIONS, DATABASE } = require("../config/mongodb.config.js");
+var MongoClient = require("mongodb").MongoClient;
 var router = require("express").Router();
 
 var createRegistData = function (body) {
@@ -56,6 +58,29 @@ router.post("/posts/regist/confirm", (req, res)=>{
     return;
   }
   res.render(__dirname + "/../views/account/posts/regist-confirm.ejs", { original });
+});
+
+router.post("/posts/regist/execute", (req, res)=>{
+  var original = createRegistData(req.body);
+  var errors = validateRegistData(req.body);
+  if(errors){
+    res.render(__dirname + "/../views/account/posts/regist-form.ejs", { errors, original });
+    return;
+  }
+
+  MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
+    var db = client.db(DATABASE);
+    db.collection("posts").insertOne(original)
+    .then(()=>{
+      res.render(__dirname + "/../views/account/posts/regist-complete.ejs");
+    }).catch((error)=>{
+      throw error;
+    }).then(()=>{
+      client.close();
+    });
+  });
+
+  
 });
 
 module.exports = router;
