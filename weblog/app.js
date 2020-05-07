e0c47939d599faa3f9574f3b8a935e874b4851ee
JIN <a1824jj@aiit.ac.jp>
2020-05-07 01:40:55 +0000

    add log4js + config

--- weblog/app.js
+++ weblog/app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+//var logger = require("./lib/log/logger").console;
 
 app.set("view engine", "ejs");
 app.disable("x-powered-by");
@@ -9,3 +10,4 @@ app.use("/public", express.static( __dirname + "/public/" + (process.env.NODE_EN
 app.use("/", require("./routes/index.js"));
 
 app.listen(3000);
+//logger.info("start.");
