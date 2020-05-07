fb5778b2404549331cc0a97d0a32a59e76db51d0
JIN <a1824jj@aiit.ac.jp>
2020-05-07 02:29:04 +0000

    log4js - accesslog && make middleware

--- weblog/app.js
+++ weblog/app.js
@@ -1,4 +1,5 @@
 var systemLogger = require("./lib/log/systemlogger");
+var accessLogger = require("./lib/log/accesslogger");
 var express = require("express");
 var app = express();
 
@@ -7,15 +8,10 @@ app.disable("x-powered-by");
 
 app.use("/public", express.static( __dirname + "/public/" + (process.env.NODE_ENV === "development" ? "development" : "production")));
 
+app.use(accessLogger());
+
 app.use("/", require("./routes/index.js"));
 
 app.use(systemLogger());
-
-//---
-//var logger = require("./lib/log/logger").application;
-// logger.addContext("key", "test");
-// logger.error("message");
-//logger.error("test", "message2");
-//---
 
 app.listen(3000);
