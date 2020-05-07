79d28ba82fe9159f0f91ebc5917ffa4622731c13
JIN <a1824jj@aiit.ac.jp>
2020-05-07 06:50:43 +0000

    new post input form

--- weblog/app.js
+++ weblog/app.js
@@ -1,5 +1,6 @@
 var systemLogger = require("./lib/log/systemlogger.js");
 var accessLogger = require("./lib/log/accesslogger.js");
+var bodyParser = require("body-parser");
 var express = require("express");
 var app = express();
 
@@ -9,6 +10,9 @@ app.disable("x-powered-by");
 app.use("/public", express.static( __dirname + "/public/" + (process.env.NODE_ENV === "development" ? "development" : "production")));
 
 app.use(accessLogger());
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use("/", require("./routes/index.js"));
 app.use("/posts/", require("./routes/posts.js"));
