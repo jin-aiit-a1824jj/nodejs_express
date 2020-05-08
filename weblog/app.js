a6ad96da14160202f29b72781226d0c7f9d5584f
JIN <a1824jj@aiit.ac.jp>
2020-05-08 03:53:15 +0000

    add custom error page

--- weblog/app.js
+++ weblog/app.js
@@ -48,8 +48,41 @@ app.use("/", (()=>{
   return router;
 })());
 
-
-
 app.use(systemLogger());
+
+app.use((req,res,next) => {
+  var data = {
+    method: req.method,
+    protocol: req.protocol,
+    version: req.httpVersion,
+    url: req.url
+  };
+  res.status(404);
+  if(req.xhr){
+    res.json(data);
+  }else{
+    res.render(__dirname + "/views/404.ejs",{data});
+  }
+});
+
+app.use((err, req, res, next) => {
+  var data = {
+    method: req.method,
+    protocol: req.protocol,
+    version: req.httpVersion,
+    url: req.url,
+    error: (process.env.NODE_ENV === "development") ? {
+      name: err.name,
+      message: err.message,
+      stack: err.stack
+    } : undefined
+  };
+  res.status(500);
+  if(req.xhr){
+    res.json(data);
+  }else{
+    res.render(__dirname + "/views/500.ejs",{data});
+  }
+});
 
 app.listen(3000);
--- weblog/routes/index.js
+++ weblog/routes/index.js
@@ -1,6 +1,7 @@
 var router = require("express").Router();
 
 router.get("/", (req, res) => {
+  throw new Error("Hello Error!");
   res.render( __dirname + "/../views/index.ejs");
 });
 
